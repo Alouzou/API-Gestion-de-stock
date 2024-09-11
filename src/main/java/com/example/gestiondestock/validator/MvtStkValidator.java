3ce68154fda94ee1990f0eb34f58300f40e0cfe2
Amine <amineouzou@gmail.com>
2024-09-11 17:32:12 -0400

    Création des validateurs

---
+++ src/main/java/com/example/gestiondestock/validator/MvtStkValidator.java
@@ -0,0 +1,37 @@
+package com.example.gestiondestock.validator;
+
+import com.example.gestiondestock.dto.MvtStkDto;
+import org.springframework.util.StringUtils;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MvtStkValidator {
+    public static List<String> validate(MvtStkDto mvtStkDto) {
+        List<String> errors = new ArrayList<>();
+        if (mvtStkDto == null) {
+            errors.add("Veuillez renseigner la date du mouvenent");
+            errors.add("Veuillez renseigner la quantite du mouvenent");
+            errors.add("Veuillez renseigner l'article");
+            errors.add("Veuillez renseigner le type du mouvement");
+
+            return errors;
+        }
+        if (mvtStkDto.getDateMvt() == null) {
+            errors.add("Veuillez renseigner la date du mouvenent");
+        }
+        if (mvtStkDto.getQuantite() == null || mvtStkDto.getQuantite().compareTo(BigDecimal.ZERO) == 0) {
+            errors.add("Veuillez renseigner la quantite du mouvenent");
+        }
+        if (mvtStkDto.getArticle() == null || mvtStkDto.getArticle().getId() == null) {
+            errors.add("Veuillez renseigner l'article");
+        }
+        //errors.addAll(ArticleValidator.validate(mvtStkDto.getArticle()));
+        if (!StringUtils.hasLength(mvtStkDto.getTypeMvt().name())) {
+            errors.add("Veuillez renseigner le type du mouvement");
+        }
+
+        return errors;
+    }
+}
---
+++ src/main/java/com/example/gestiondestock/validator/VentesValidator.java
@@ -0,0 +1,27 @@
+package com.example.gestiondestock.validator;
+
+import com.example.gestiondestock.dto.VentesDto;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VentesValidator {
+    public static List<String> validate(VentesDto ventesDto) {
+        List<String> errors = new ArrayList<>();
+        if (ventesDto == null) {
+            errors.add("Veuillez renseigner le code de la commande");
+            errors.add("Veuillez renseigner la date de la commande");
+            return errors;
+        }
+
+        if (!StringUtils.hasLength(ventesDto.getCode())) {
+            errors.add("Veuillez renseigner le code de la commande");
+        }
+        if (ventesDto.getDateVente() == null) {
+            errors.add("Veuillez renseigner la date de la commande");
+        }
+
+        return errors;
+    }
+}
