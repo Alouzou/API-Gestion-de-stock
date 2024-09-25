8ed415d2714da0770b82ad4e36c56589363cc34e
Amine <amineouzou@gmail.com>
2024-09-24 22:35:25 -0400

    Ajout API Flickr

---
+++ src/main/java/com/example/gestiondestock/services/FlickrService.java
@@ -0,0 +1,11 @@
+package com.example.gestiondestock.services;
+
+import com.flickr4java.flickr.FlickrException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+
+public interface FlickrService {
+    String savePhoto(InputStream photo, String title) throws FlickrException, IOException, ExecutionException, InterruptedException;
+}
--- src/main/java/com/example/gestiondestock/services/VenteService.java
+++ src/main/java/com/example/gestiondestock/services/VenteService.java
@@ -6,12 +6,8 @@ import java.util.List;
 
 public interface VenteService {
     VentesDto save(VentesDto dto);
-
     VentesDto findById(Integer id);
-
     VentesDto findByCode(String code);
-
     List<VentesDto> findAll();
-
     void delete(Integer id);
 }
